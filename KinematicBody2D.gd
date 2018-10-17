7b887c156047a1d711065a64ea2cdc40ca9be5ea
mdeniaud <mathieu.deniaud@crossknowledge.com>
2018-10-17 17:51:45 +0200

    Add tileset and player animation

--- KinematicBody2D.gd
+++ KinematicBody2D.gd
@@ -4,8 +4,9 @@ extends KinematicBody2D
 # var a = 2
 # var b = "textvar"
 const UP = Vector2(0, -1)
-const SPEED = 200
+const MAX_SPEED = 200
 const GRAVITY = 20
+const ACCELERATION = 50
 const JUMP_HEIGHT = -500
 var motion = Vector2()
 
@@ -21,24 +22,32 @@ func _ready():
 
 func _physics_process(delta):
 	motion.y += GRAVITY
+	var friction = false
 	
 	if Input.is_action_pressed("ui_right"):
-		motion.x = SPEED
+		motion.x = min(motion.x + ACCELERATION, MAX_SPEED)
 		$Sprite.flip_h = false
 		$Sprite.play("Run")
 	elif Input.is_action_pressed("ui_left"):
-		motion.x = -SPEED
+		motion.x = max(motion.x - ACCELERATION, -MAX_SPEED)
 		$Sprite.flip_h = true
 		$Sprite.play("Run")
 	else:
-		motion.x = 0
+		friction = true
 		$Sprite.play("Idle")
 		
 	if is_on_floor():
 		if Input.is_action_just_pressed("ui_up"):
 			motion.y = JUMP_HEIGHT
+		if friction:
+			motion.x = lerp(motion.x, 0, 0.2)
 	else:
-		$Sprite.play("Jump")
+		if motion.y < 0:
+			$Sprite.play("Jump")
+		else:
+			$Sprite.play("Fall")
+		if friction:
+			motion.x = lerp(motion.x, 0, 0.05)
 		
 	motion = move_and_slide(motion, UP)
 	
